4a2e20af0823e551871e684d5916f75a6fad1f03
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2024-02-05 15:29:53 +0300

    feat: add checking error in print functions

--- operations/printOperations.js
+++ operations/printOperations.js
@@ -1,18 +1,30 @@
 import os from "os";
 
 function printHomedir() {
-  const homedir = os.homedir();
-  console.log(`Home directory: ${homedir}`);
+  try {
+    const homedir = os.homedir();
+    console.log(`Home directory: ${homedir}`);
+  } catch (error) {
+    console.error(`Error getting home directory: ${error.message}`);
+  }
 }
 
 function printUsername() {
-  const userInfo = os.userInfo();
-  console.log(`Current system username: ${userInfo.username}`);
+  try {
+    const userInfo = os.userInfo();
+    console.log(`Current system username: ${userInfo.username}`);
+  } catch (error) {
+    console.error(`Error getting system username: ${error.message}`);
+  }
 }
 
 function printArchitecture() {
-  const architecture = process.arch;
-  console.log(`Node.js binary compiled for architecture: ${architecture}`);
+  try {
+    const architecture = process.arch;
+    console.log(`Node.js binary compiled for architecture: ${architecture}`);
+  } catch (error) {
+    console.error(`Error getting architecture information: ${error.message}`);
+  }
 }
 
 export { printArchitecture, printHomedir, printUsername };
